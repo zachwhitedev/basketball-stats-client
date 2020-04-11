d44243acb22a0752872710bef398339c906666ed
Zach White <zechnwhite@gmail.com>
2020-04-10 21:21:40 -0700

    game events being logged to UI

--- src/dashboards/GameActive/Cards/HistoryCard/HistoryCard.js
+++ src/dashboards/GameActive/Cards/HistoryCard/HistoryCard.js
@@ -2,15 +2,62 @@ import React, { useState, useEffect } from 'react';
 import './HistoryCard.css';
 
 export default function HistoryCard(props){
-  const { tempEvent } = props;
-  const[eventLog, updateLog] = useState([{ name: 'Test' }]);
+  const { tempEvent, players } = props;
+  const[eventLog, updateLog] = useState([]);
 
   useEffect(() => {
-    let description = ''
-    if(tempEvent.eventid == 1){
-      description = 'Lebron James!'
-    }
-    updateLog([...eventLog, { name: description }])
+    console.log('useEffect, HistoryCard.js')
+    if(tempEvent.playerid && players.length > 0){
+      let description = '';
+      let playername = '';
+      switch(tempEvent.eventid){
+        case 1:
+          description = 'Made 2pt shot.'
+          break;
+        case 2:
+          description = 'Missed 2pt shot.'
+          break;
+        case 3:
+          description = 'Made 3pt shot.'
+          break;
+        case 4:
+          description = 'Missed 3pt shot.'
+          break;
+        case 5:
+          description = 'Made free throw.'
+          break;
+        case 6:
+          description = 'Missed free throw.'
+          break;
+        case 7:
+          description = 'Got a rebound.'
+          break;
+        case 8:
+          description = 'Made an assist.'
+          break;
+        case 9:
+          description = 'Made a block.'
+          break;
+        case 10:
+          description = 'Got a steal.'
+          break;
+        case 11:
+          description = 'Turned the ball over.'
+          break;
+        case 12:
+          description = 'Made a foul.'
+          break;
+        default:
+          break;
+      }
+      for(let i=0; i < players.length; i++){
+        if(tempEvent.playerid == players[i].id){
+          playername = players[i].firstname;
+          updateLog([...eventLog, { player: playername, name: description }])
+          return;
+        } else continue;
+      }
+    } else return;
   }, [tempEvent.eventid])
 
     return(
@@ -18,7 +65,7 @@ export default function HistoryCard(props){
         <h3>Event Log</h3>
         {eventLog.map(item => {
           return(
-            <p>{item.name}</p>
+            <p>{item.player + ' - ' + item.name}</p>
           )
         })}
       </div>
