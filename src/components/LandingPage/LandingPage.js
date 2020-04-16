77ceda87fcded83c474f73a8766ec35315cfd926
Zach White <zechnwhite@gmail.com>
2020-04-15 18:04:28 -0700

    begins landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import './LandingPage.css';
 import { Link } from 'react-router-dom';
+import mrkt1 from '../../assets/img/mrkt.png';
 
 import Navbar from '../Navbar/index';
 
@@ -9,7 +10,10 @@ export default function LandingPage(props) {
     <div id='landingpage-container'>
       <Navbar />
       <div id='landingpage-content'>
-        Marketing materials go here.
+        <h1>Slam Dunk Stats</h1>
+        <h3>The internet's best web app for tracking and storing basketball game statistics in real time.</h3>
+        <p>Create teams, add players, start games, and record all the important stats you need.</p>
+        <img id='main-img' src={mrkt1}></img>
       </div>
     </div>
   );
